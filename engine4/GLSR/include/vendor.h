ed86d61dca4326d4d22bd48fee1ed1fb8a04c82a
Saki <Stanislaw.Darowski101@gmail.com>
2022-03-08 00:20:11 +0100

    _fix includes

--- engine4/GLSR/include/vendor.h
+++ engine4/GLSR/include/vendor.h
@@ -2,14 +2,12 @@
 
 // glew
 #define GLEW_STATIC
-#include <glew/glew-2.1.0/include/GL/glew.h>
-
 #include <GL/glew.h>
 
 // glfw
-#include <glfw/include/GLFW/glfw3.h>
+#include <GLFW/glfw3.h>
 
 // glm
-#include <glm/glm-0.9.9.8/include/glm/glm.hpp>
-#include <glm/glm-0.9.9.8/include/glm/gtc/matrix_transform.hpp>
-#include <glm/glm-0.9.9.8/include/glm/gtc/type_ptr.hpp>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
